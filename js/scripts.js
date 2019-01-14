b34a744ef3276696cda1dbdb4a08dc89d3dc76ec
Aaron Taylor and Young Liu <young@>
2019-01-14 13:22:29 -0800

    add a function to capitalize the fist and last letter.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
-
+var capFirstLast = function(word) {
+  word = word.replace(word.charAt(0), word.charAt(0).toUpperCase());
+  word = word.substring(0, word.length - 1) + word.charAt(word.length - 1).toUpperCase();
+  return word;
+};
 
 
 
 var word = prompt("Please enter a sentence.");
-alert(word);
+alert(capFirstLast(word));
