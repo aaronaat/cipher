17c221af9e94b8ef8773da14483bf595e31b07b8
Aaron Taylor and Young Liu <aaron@>
2019-01-14 14:54:53 -0800

    finish the cipher webpage.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
-var capFirstLast = function(word) {
-  word = word.replace(word.charAt(0), word.charAt(0).toUpperCase());
-  word = word.substring(0, word.length - 1) + word.charAt(word.length - 1).toUpperCase();
-  return word;
+var capFirstLast = function(word){
+  var letter1 = word.charAt(0).toUpperCase();
+  var letter2 = word.charAt(word.length - 1).toUpperCase();
+  return letter1 + letter2;
 };
 
+var reverseOrder = function(word){
+  return word.charAt(1) + word.charAt(0);
+};
 
+var thirdFunction = function(word){
+
+  return word + reverseOrder(capFirstLast(word));
+};
+
+var countDivide = function(word){
+   return word.charAt(word.length / 2) + word;
+};
+
+var mirror = function(word){
+  var m = word.split();
+  return m.reverse();
+};
 
 var word = prompt("Please enter a sentence.");
-alert(capFirstLast(word));
+alert(countDivide(thirdFunction(word)));
